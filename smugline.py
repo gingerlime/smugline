8ca2cdca80175eeacd5e456c27c46c0d3ea61be0
Yoav <yoav@gingerlime.com>
2014-12-30 11:01:38 +0100

    fixes #10 - adding gif image support

--- smugline.py
+++ smugline.py
@@ -59,9 +59,9 @@ import json
 import requests
 from itertools import groupby
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
-IMG_FILTER = re.compile(r'.+\.(jpg|png|jpeg|tif|tiff)$', re.IGNORECASE)
+IMG_FILTER = re.compile(r'.+\.(jpg|png|jpeg|tif|tiff|gif)$', re.IGNORECASE)
 VIDEO_FILTER = re.compile(r'.+\.(mov|mp4|avi|mts)$', re.IGNORECASE)
 ALL_FILTER = re.compile('|'.join([IMG_FILTER.pattern, VIDEO_FILTER.pattern]),
                         re.IGNORECASE)
